586021dd111d1401d4d46895c9d12d18733e055f
millinor <45846199+millinor@users.noreply.github.com>
2018-12-14 15:11:55 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,15 @@
+void vowel.or consonant(charx)
+{
+if(x=='a'//x=='e'//x=='i'//x=='o'//x=='u')
+count<<"vowel"<<end1;
+else
+count<<"consonant"<<end1;
+else
+count<<"consonant"<<end1
+}
+intmain()
+{
+vowel or consonant("c");
+vowel or consonant("e")
+return 0;
+}
